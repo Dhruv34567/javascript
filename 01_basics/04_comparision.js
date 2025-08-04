4cbea205b5a8dded68cd91f1287a83bb69dfc890
Dhruv <157515651+Dhruv34567@users.noreply.github.com>
2025-08-04 15:17:01 +0000

    datatypeSummary

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log("checking for null")
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log("checking for undefined")
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+// console.log("2" === 2);
